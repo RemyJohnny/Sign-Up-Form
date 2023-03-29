4dc0c39597a74bb8f4c6709b6584bef0258b19e6
RemyJohnny <ewerejohnnyjnr93@gmail.com>
2023-03-30 01:17:16 +0300

    complete project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sign-Up-Form
 intermediate HTML and CSS Course
+
+project from  the odin project to make a Sign Up form for a fake online service 
---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('i love you');
+const CreateAccount = document.querySelector('.createAccount');
+const passwordInput = document.querySelector('#Password');
+const passwordInput2 = document.querySelector('#ConfirmPassword');
+const errorMessage = document.querySelectorAll('.error');
+
+
+CreateAccount.addEventListener('click', function(e) {
+  if (passwordInput.value !== passwordInput2.value){
+    errorMessage.forEach(span => {
+        span.textContent = 'your password does not match'
+    });
+    passwordInput.setCustomValidity('invalid')
+    passwordInput2.setCustomValidity('invalid')
+    passwordInput2.value = '';
+    passwordInput2.focus()
+    console.log(e);
+    e.preventDefault();
+  }
+  else{
+    passwordInput.setCustomValidity('')
+    passwordInput2.setCustomValidity('')
+    errorMessage.forEach(span => {
+        span.innerHTML = '';
+    });
+  }
+
+
+})
